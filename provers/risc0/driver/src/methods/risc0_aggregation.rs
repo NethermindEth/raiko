8edb71c676171d26a27a9f56a9795905dcab09f7
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-11-28 18:39:33 +0000

    risc0 local prover

--- provers/risc0/driver/src/methods/risc0_aggregation.rs
+++ provers/risc0/driver/src/methods/risc0_aggregation.rs
@@ -1,5 +1,3 @@
-pub const RISC0_AGGREGATION_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation");
-pub const RISC0_AGGREGATION_ID: [u32; 8] = [
-    3597901240, 347325416, 3317656102, 3291080048, 86415258, 1347371281, 3482575988, 4094302300,
-];
+
+pub const RISC0_AGGREGATION_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation");
+pub const RISC0_AGGREGATION_ID: [u32; 8] = [113476908, 3893617259, 2481783391, 1943310844, 3257570539, 1848043955, 3743040806, 2589085688];
--- provers/risc0/driver/src/methods/risc0_guest.rs
+++ provers/risc0/driver/src/methods/risc0_guest.rs
@@ -1,5 +1,3 @@
-pub const RISC0_GUEST_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
-pub const RISC0_GUEST_ID: [u32; 8] = [
-    2705224968, 672422473, 3589767632, 3895344282, 3642477750, 1142566656, 2251137472, 1131663031,
-];
+
+pub const RISC0_GUEST_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
+pub const RISC0_GUEST_ID: [u32; 8] = [1819220236, 1367897493, 57189654, 4166868772, 1849512468, 912547844, 2423900601, 926308460];
