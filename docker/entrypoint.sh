f495c109403b24ff54e74a647bef46639c0c0042
Alexander Fedorovskyi <kikban030@gmail.com>
2025-09-23 11:48:50 +0200

    feat: sgx docker updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,11 +16,8 @@ RAIKO_APP_DIR=${RAIKO_APP_DIR:-"/opt/raiko/bin"}
 RAIKO_CONF_DIR=${RAIKO_CONF_DIR:-"/etc/raiko"}
 RAIKO_CONF_BASE_CONFIG="$RAIKO_CONF_DIR/$BASE_CONFIG_FILE"
 RAIKO_CONF_CHAIN_SPECS="$RAIKO_CONF_DIR/$BASE_CHAINSPEC_FILE"
-DEVNET_CHAINSPEC_FILE=$RAIKO_CONF_DIR/chain_spec_list_devnet.json
-PRODUCT_CHAINSPEC_FILE=$RAIKO_CONF_DIR/chain_spec_list_default.json
 RAIKO_GUEST_APP_FILENAME="sgx-guest"
 GAIKO_GUEST_APP_FILENAME="gaiko"
-RAIKO_GUEST_SETUP_FILENAME="raiko-setup"
 RAIKO_INPUT_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.docker.manifest.template"
 RAIKO_OUTPUT_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.manifest.sgx"
 RAIKO_SIGNED_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.sig"
@@ -67,84 +64,11 @@ function bootstrap() {
     cd -
 }
 
-function bootstrap_with_self_register() {
-    L1_NETWORK="${L1_NETWORK:-holesky}"
-    NETWORK="${NETWORK:-taiko_a7}"
-    mkdir -p "$RAIKO_DOCKER_VOLUME_SECRETS_PATH"
-    cd "$RAIKO_APP_DIR"
-    ./$RAIKO_GUEST_SETUP_FILENAME bootstrap --l1-network $L1_NETWORK --network $NETWORK --chain-spec-path $RAIKO_CONF_CHAIN_SPECS
-    cd -
-}
 
-function update_chain_spec_json() {
-    CONFIG_FILE=$1
-    CHAIN_NAME=$2
-    KEY_NAME=$3
-    UPDATE_VALUE=$4
-    jq \
-        --arg update_value "$UPDATE_VALUE" \
-        --arg chain_name "$CHAIN_NAME" \
-        --arg key_name "$KEY_NAME" \
-        'map(if .name == $chain_name then .[$key_name] = $update_value else . end)' $CONFIG_FILE \
-        >/tmp/config_tmp.json && mv /tmp/config_tmp.json $CONFIG_FILE
-    echo "Updated $CONFIG_FILE $CHAIN_NAME.$KEY_NAME=$UPDATE_VALUE"
-}
 
-function update_docker_chain_specs() {
-    CONFIG_FILE=$1
-    if [ ! -f $CONFIG_FILE ]; then
-        echo "$BASE_CHAINSPEC_FILE file not found."
-        return 1
-    fi
 
-    if [ -n "${ETHEREUM_RPC}" ]; then
-        update_chain_spec_json $CONFIG_FILE "ethereum" "rpc" $ETHEREUM_RPC
-    fi
 
-    if [ -n "${ETHEREUM_BEACON_RPC}" ]; then
-        update_chain_spec_json $CONFIG_FILE "ethereum" "beacon_rpc" $ETHEREUM_BEACON_RPC
-    fi
-
-    if [ -n "${HOLESKY_RPC}" ]; then
-        update_chain_spec_json $CONFIG_FILE "holesky" "rpc" $HOLESKY_RPC
-    fi
-
-    if [ -n "${HOLESKY_BEACON_RPC}" ]; then
-        update_chain_spec_json $CONFIG_FILE "holesky" "beacon_rpc" $HOLESKY_BEACON_RPC
-    fi
-
-    if [ -n "${TAIKO_A7_RPC}" ]; then
-        update_chain_spec_json $CONFIG_FILE "taiko_a7" "rpc" $TAIKO_A7_RPC
-    fi
-
-    if [ -n "${TAIKO_MAINNET_RPC}" ]; then
-        update_chain_spec_json $CONFIG_FILE "taiko_mainnet" "rpc" $TAIKO_MAINNET_RPC
-    fi
-}
-
-function update_config_json() {
-    CONFIG_FILE=$1
-    KEY_NAME=$2
-    UPDATE_VALUE=$3
-    jq \
-        --arg update_value "$UPDATE_VALUE" \
-        --arg key_name "$KEY_NAME" \
-        '.[$key_name] = $update_value' $CONFIG_FILE \
-        >/tmp/config_tmp.json && mv /tmp/config_tmp.json $CONFIG_FILE
-    echo "Updated $CONFIG_FILE $CHAIN_NAME.$KEY_NAME=$UPDATE_VALUE"
-}
-
-function update_raiko_network() {
-    CONFIG_FILE=$1
-    if [ -n "${L1_NETWORK}" ]; then
-        update_config_json $CONFIG_FILE "l1_network" $L1_NETWORK
-    fi
-
-    if [ -n "${NETWORK}" ]; then
-        update_config_json $CONFIG_FILE "network" $NETWORK
-    fi
-}
-
+# Convienience function to update raiko config with SGX instance ids from env vars
 function update_raiko_sgx_instance_id() {
     CONFIG_FILE=$1
     if [[ -n $SGX_INSTANCE_ID ]]; then
@@ -177,26 +101,6 @@ function update_raiko_sgx_instance_id() {
     fi
 }
 
-# merge devnet & product chain spec here
-function merge_json_arrays() {
-    local input1="$1"
-    local input2="$2"
-    local output="$3"
-
-    if [[ ! -f "$input1" || ! -f "$input2" ]]; then
-        echo "❌ $input1 or $input2 not found！"
-        return 1
-    fi
-
-    jq -s 'add' "$input1" "$input2" >"$output"
-
-    if [[ $? -eq 0 ]]; then
-        echo "✅ merge chainspec success to: $output"
-    else
-        echo "❌ merge failed!"
-        return 1
-    fi
-}
 
 if [[ -z "${PCCS_HOST}" ]]; then
     MY_PCCS_HOST=pccs:8081
@@ -225,12 +129,6 @@ if [[ -n $SGX ]]; then
     if [[ $# -eq 1 && $1 == "--init" ]]; then
         echo "start bootstrap"
         bootstrap
-    elif [[ $# -eq 1 && $1 == "--init-self-register" ]]; then
-        echo "start bootstrap with self register"
-        #merge chain spec to a all-in-one file
-        merge_json_arrays $PRODUCT_CHAINSPEC_FILE $DEVNET_CHAINSPEC_FILE $RAIKO_CONF_CHAIN_SPECS
-        update_docker_chain_specs $RAIKO_CONF_CHAIN_SPECS
-        bootstrap_with_self_register
     else
         echo "start proving"
         if [ "$SGX_MODE" = "local" ]; then
@@ -247,45 +145,10 @@ if [[ -n $SGX ]]; then
         fi
 
         #update raiko server config
-        update_raiko_network $RAIKO_CONF_BASE_CONFIG
         update_raiko_sgx_instance_id $RAIKO_CONF_BASE_CONFIG
 
-        #merge chain spec to a all-in-one file
-        merge_json_arrays $PRODUCT_CHAINSPEC_FILE $DEVNET_CHAINSPEC_FILE $RAIKO_CONF_CHAIN_SPECS
         update_docker_chain_specs $RAIKO_CONF_CHAIN_SPECS
 
         /opt/raiko/bin/raiko-host --config-path=$RAIKO_CONF_BASE_CONFIG --chain-spec-path=$RAIKO_CONF_CHAIN_SPECS "$@"
-    fi
-fi
-
-if [[ -n $ZK ]]; then
-    echo "running raiko in zk mode"
-    if [ ! -f $RAIKO_CONF_BASE_CONFIG ]; then
-        echo "$RAIKO_CONF_BASE_CONFIG file not found."
-        exit 1
-    fi
-
-    /opt/raiko/bin/raiko-host  --config-path=$RAIKO_CONF_BASE_CONFIG --chain-spec-path=$RAIKO_CONF_CHAIN_SPECS "$@"
-fi
-
-if [[ -n $SGX_SERVER ]]; then
-    echo "running sgx in sgx server mode"
-
-    if [[ $# -eq 1 && $1 == "--init" ]]; then
-        echo "start server bootstrap"
-        # useless here, as it can share same raiko init
-        # keep it now for future refactory
-        bootstrap
-    else
-        if [[ -z $SGX_PACAYA_INSTANCE_ID || -z $SGXGETH_PACAYA_INSTANCE_ID ]]; then 
-            echo "SGX_PACAYA_INSTANCE_ID and SGXGETH_PACAYA_INSTANCE_ID must be presented, please check."
-            exit 1
-        fi
-
-        echo "start sgx-guest --sgx-instance-id $SGX_PACAYA_INSTANCE_ID --address 0.0.0.0 --port 9090"
-        gramine-sgx /opt/raiko/bin/sgx-guest serve --sgx-instance-id $SGX_PACAYA_INSTANCE_ID --address 0.0.0.0 --port 9090 | sed 's/^/[raiko] /' &
-        echo "start gaiko serve --sgx-instance-id $SGXGETH_PACAYA_INSTANCE_ID --port 8080"
-        /opt/raiko/bin/gaiko --verbosity $GAIKO_GUEST_APP_VERBOSE_LEVEL serve --sgx-instance-id $SGXGETH_PACAYA_INSTANCE_ID --port 8090 | sed 's/^/[gaiko] /' &
-        wait
     fi
 fi
