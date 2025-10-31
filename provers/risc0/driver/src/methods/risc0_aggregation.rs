52a392ff9b8c136c192ee0d952c86cb3e5b02c8d
Alexander Fedorovskyi <kikban030@gmail.com>
2025-10-31 12:48:39 +0100

    feat: update risc0 Cargo.lock

--- provers/risc0/driver/src/methods/risc0_aggregation.rs
+++ provers/risc0/driver/src/methods/risc0_aggregation.rs
@@ -1,5 +1,3 @@
-pub const RISC0_AGGREGATION_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation.bin");
-pub const RISC0_AGGREGATION_ID: [u32; 8] = [
-    1763512149, 1614382383, 1643566830, 1781897433, 4278195220, 137913589, 1428428252, 2480345784,
-];
+
+pub const RISC0_AGGREGATION_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation.bin");
+pub const RISC0_AGGREGATION_ID: [u32; 8] = [1329582374, 2278708978, 653712996, 156129797, 1690706817, 2583955636, 2759656662, 1053843228];
--- provers/risc0/driver/src/methods/risc0_batch.rs
+++ provers/risc0/driver/src/methods/risc0_batch.rs
@@ -1,5 +1,3 @@
-pub const RISC0_BATCH_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
-pub const RISC0_BATCH_ID: [u32; 8] = [
-    670699829, 1831509791, 3346729772, 399557171, 2847537917, 102228968, 2530887920, 451262675,
-];
+
+pub const RISC0_BATCH_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
+pub const RISC0_BATCH_ID: [u32; 8] = [2498296093, 1461333, 692339452, 3225677847, 2837865185, 2366763363, 2101650258, 3646774563];
